23a100116c602e320cf8067bfadb6f3baa9ecec5
KrisPy1729 <148775678+KrisPy1729@users.noreply.github.com>
2025-02-09 21:36:26 +0000

    Update divvy_streamlit.py
    Added maps visualization using folium, st_folium, cached the dashboard creation and maps creation using st.cache_data decorator to prevent the map from reloading with every user interaction.

--- divvy_streamlit.py
+++ divvy_streamlit.py
@@ -1,56 +1,132 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import folium
+from folium.plugins import MarkerCluster
+from streamlit_folium import st_folium
 from divvy_streamlit_functions import *
 
-def main():
-    """ Main guy DUH! """
-    
-    divvy_gbfs_feed = 'https://gbfs.divvybikes.com/gbfs/2.3/gbfs.json'
-    
-    # let's get the json response from the url
-    divvy_data = get_data(divvy_gbfs_feed)
-    
-    # Title of the dashboard
-    st.title("Divvy BikeShare") 
-    
-    # Generates error response if no data loaded either due to server side error or any type of error in connecting to server
+
+# Caching the data fetching function
+@st.cache_data
+def get_divvy_data(url):
+    """ Fetching data using the function containing the requests library """
+    return get_data(url)
+
+
+
+
+
+# Caching the dataframe creation and data processing
+@st.cache_data
+def create_station_dashboard(divvy_data, lang):
+    """Process and create the station dashboard data."""
+    # Display title
+    st.title("Divvy Bike Dashboard")
+
     if divvy_data is None:
-        st.error("Failed to load data from the API")
-        return
+        return None
     
-    # Materialises user clickable language buttons in streamlit, sets the default language if the user is a lazy English speaking monolingual
-    lang = st_lang_buttons()
-    
-    
-    # Gets that language specific part of the json response
     divvy_data_lang = get_data_lang(divvy_data, lang)
     
-    # If the language specific data exists then sends it to extractor to get name, url pairs inside the json, then further get json responses from those urls into a dictionary
     if divvy_data_lang:
         divvy_feeds_json = feeds_extractor(divvy_data_lang['feeds'])
     else:
-        # Oh well, the url has feeds only in some exotic language unknown to Americans
         st.warning("The feed doesn't contain data in any defined languages")
-        return
+        return None
     
-    # Passes station_information to a dataframe using json_normalize
+    # Normalizing and flattening the JSONs into dataframes
     station_information_df = pd.json_normalize(divvy_feeds_json['station_information']['stations'])
-    # Passes station_status to a dataframe using json_normalize, the array has a list of dictionaries vehicle_types_available, so using record_path and meta to flatten it.
     station_status_df = pd.json_normalize(divvy_feeds_json['station_status']['stations'], record_path='vehicle_types_available', 
                                           meta=['num_docks_available', 'num_bikes_available', 'is_renting', 'is_installed', 
                                                 'num_docks_disabled', 'num_scooters_available', 'num_bikes_disabled', 'last_reported', 
                                                 'is_returning', 'num_ebikes_available', 'station_id', 'num_scooters_unavailable'], errors='ignore')
+    station_status_df['vehicle_type_id'] = station_status_df['vehicle_type_id'].astype(int)
     
-    # Passes free_bike_status to a dataframe using json_normalize
     free_bike_status_df = pd.json_normalize(divvy_feeds_json['free_bike_status']['bikes'])
+    vehicle_types_df = pd.json_normalize(divvy_feeds_json['vehicle_types']['vehicle_types'])
+    vehicle_types_df['vehicle_type_id'] = vehicle_types_df['vehicle_type_id'].astype(int)
     
-    # using st.write to write the tables to the dashboard to verify
-    st.write(station_information_df.head())
-    st.write(station_status_df.head())
-    st.write(free_bike_status_df.head())
+    pricing_plans_df = pd.json_normalize(divvy_feeds_json['system_pricing_plans']['plans'], record_path='per_min_pricing', 
+                                         meta=['price', 'currency', 'description', 'name', 'plan_id', 'is_taxable'], errors='ignore')
+    
+    # Merging and processing the vehicle data
+    available_vehicle_count = station_status_df[['station_id', 'vehicle_type_id', 'count']]
+    vehicles_df = pd.merge(available_vehicle_count, vehicle_types_df, on='vehicle_type_id', how='left')
+    
+    vehicles_df_conditions = [(vehicles_df['vehicle_type_id'] == 1), (vehicles_df['vehicle_type_id'] == 2), (vehicles_df['vehicle_type_id'] == 3)]
+    vehicles_df_choices = ["Bike", "E-Bike", "E-Scooter"]
+    vehicles_df['Category'] = np.select(vehicles_df_conditions, vehicles_df_choices, default="Other")
+    vehicles_df = vehicles_df[['station_id', 'Category', 'count']]
+    
+    vehicles_df_pivoted = pd.pivot_table(vehicles_df, index='station_id', columns=['Category'], values='count', aggfunc='sum', fill_value=0)
+    vehicles_df_pivoted.columns = [f'available_{col}' for col in vehicles_df_pivoted.columns]
+    vehicles_df_pivoted.reset_index(inplace=True)
+    vehicle_status = vehicles_df_pivoted
+    
+    # Merging and cleaning the dataframes (just like before)
+    station_status_df.drop(columns=['vehicle_type_id', 'count'], inplace=True)
+    station_status_df.drop_duplicates(subset=['station_id'], inplace=True)
+    station_status = pd.merge(station_status_df, vehicle_status, on='station_id', how='left')
+    
+    station_dashboard_df = pd.merge(station_status, station_information_df, on='station_id', how='left')
+    station_dashboard_df = station_dashboard_df[['station_id', 'name', 'short_name', 'lat', 'lon', 'capacity', 'region_id', 'address', 
+                                                 'is_installed', 'is_returning', 'is_renting', 'num_docks_available', 'available_Bike', 
+                                                 'available_E-Bike',  'available_E-Scooter', 'last_reported']]
+    station_dashboard_df['last_reported'] = pd.to_datetime(station_dashboard_df['last_reported'], unit='s')
+
+    return station_dashboard_df
+
+
+# Caching the map creation process
+@st.cache_data
+def create_map(station_dashboard_df):
+    """Create the folium map."""
+    map = folium.Map(location=[41.881832, -87.623177], zoom_start=10)
+    # folium.TileLayer('cartodbdark_matter').add_to(map)
+    mc = MarkerCluster()
+
+    for index, row in station_dashboard_df.iterrows():
+        tooltip_text = "<br>".join([ f"📌{row['name']}", f"Available empty docks: {row['num_docks_available']} 🅿️", 
+                                    f"Available Bikes: {row['available_Bike']} 🚲", 
+                                    f"Available E-Bikes: {row['available_E-Bike']} 🚲⚡",
+                                    f"Available E-Scooters: {row['available_E-Scooter']} 🛴⚡"
+                                   ])
+        marker = folium.Marker(
+            location=[row['lat'], row['lon']], 
+            tooltip= tooltip_text,
+            icon=folium.Icon(color='red', icon='info-sign')
+        )
+
+            
+        mc.add_child(marker)
+    map.add_child(mc)
+    return map
+
+
+def main():
+    """Main function for the dashboard."""
+        
+    # Fetching the data from the URL
+    divvy_gbfs_feed = 'https://gbfs.divvybikes.com/gbfs/2.3/gbfs.json'
+    divvy_data = get_divvy_data(divvy_gbfs_feed)
+    
+    # Materialize user clickable language buttons in streamlit, sets the default language if the user is a lazy English speaking monolingual
+    lang = st_lang_buttons()
+    
+    # Creating stations dashboard
+    station_dashboard_df = create_station_dashboard(divvy_data, lang)
+
+    if station_dashboard_df is None:
+        st.error("Failed to load data!")
+        return
+    
+    # Creating the map (cached)
+    map = create_map(station_dashboard_df)
+
+    # Displaying the map using streamlit_folium
+    st_folium(map, height=500, width=700, returned_objects=[])
+
 
 if __name__ == "__main__":
-    # Will help later in functional testing.... or so I think
     main()
-        
-    
